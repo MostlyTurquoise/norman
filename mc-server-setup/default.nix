958b14aca69f62ac25998f7558319c4360315e7a
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2024-11-27 17:51:08 +0000

    move back to non-declarative whitelist. debug

--- mc-server-setup/default.nix
+++ mc-server-setup/default.nix
@@ -23,11 +23,8 @@
           enforce-whitelist=true;
           view-distance=20;
         };
-        whitelist = {
-          MostlyTurquoise = "665e2c56-0339-4990-9c78-400692a7ba84";
-        };
         symlinks = {
-          mods = pkgs.linkFarmFromDrvs "mods" import ./mods.nix;
+          mods = pkgs.linkFarmFromDrvs "mods" (trace (import ./mods.nix) [ builtins.fetchurl { url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/Zp9iAZdZ/fabric-api-0.110.0%2B1.21.1.jar"; sha256 = "145msngqayqw3n0i5fl6lwqdbh91qakllcx43bvp879b3a5hi408"; } ]);
         };
       };
     };
