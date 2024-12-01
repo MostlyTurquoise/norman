d69725bad6c5bd4a4cc621032fd86b25abc91bc3
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2024-12-01 10:06:55 +0000

    lithium hash

--- mc-server-setup/mods.nix
+++ mc-server-setup/mods.nix
@@ -4,7 +4,7 @@
     _Collective = builtins.fetchurl { url = "https://cdn.modrinth.com/data/e0M1UDsY/versions/nwmUrrgY/collective-1.21.1-7.87.jar"; sha256 = ""; };
     DeathBackup = builtins.fetchurl { url = "https://cdn.modrinth.com/data/Ot5JFxuv/versions/Zq5GkqAd/deathbackup-1.21.1-3.4.jar"; sha256 = "0ca6f5fxh9g5gbpd0iybzbgx5ihjc0cizhs05mqfxzbmzqxz6cbh"; };
     ItemRenameOLD = builtins.fetchurl { url = "https://cdn.modrinth.com/data/i5cjb3PQ/versions/qJaQ6MwF/itemrename-1.0.3%2B1.21.jar"; sha256 = "0si51h2h7spmnzi0x18f188qvhx73c6kbzr2iclrmyflpx0kqf85"; name="itemrename-1.0.32B1.21.jar";};
-    Lithium = builtins.fetchurl { url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/frXUdgvL/lithium-fabric-0.14.2-snapshot%2Bmc1.21.1-build.90.jar"; sha256 = ""; name = "ithium-fabric-0.14.2-snapshot2Bmc1.21.1-build.90.jar"; };
+    Lithium = builtins.fetchurl { url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/frXUdgvL/lithium-fabric-0.14.2-snapshot%2Bmc1.21.1-build.90.jar"; sha256 = "115a6dham2bnwwqsljp68za3vwb9r9zql7dbdk5d0xdgsq6vzb2f"; name = "lithium-fabric-0.14.2-snapshot2Bmc1.21.1-build.90.jar"; };
     Spark = builtins.fetchurl { url = "https://cdn.modrinth.com/data/l6YH9Als/versions/cALUj9l1/spark-1.10.109-fabric.jar"; sha256 = ""; };
     SimpleVoiceChat = builtins.fetchurl { url = "https://cdn.modrinth.com/data/9eGKb6K1/versions/Fku4RjPN/voicechat-fabric-1.21.1-2.5.26.jar"; sha256 = ""; };
     Xaero'sMiniMap = builtins.fetchurl { url = "https://cdn.modrinth.com/data/1bokaNcj/versions/C6gFqr7V/Xaeros_Minimap_24.6.1_Fabric_1.21.jar"; sha256 = ""; };
