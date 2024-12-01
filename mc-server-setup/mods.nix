44d12c6c1f625a29b9192d884a3c3abbc5bc7a40
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2024-12-01 10:10:01 +0000

    other hashes

--- mc-server-setup/mods.nix
+++ mc-server-setup/mods.nix
@@ -1,12 +1,12 @@
 {
     FabricAPI = builtins.fetchurl { url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/Zp9iAZdZ/fabric-api-0.110.0%2B1.21.1.jar"; sha256 = "145msngqayqw3n0i5fl6lwqdbh91qakllcx43bvp879b3a5hi408"; name = "fabric-api-0.110.02B1.21.1.jar"; };
     AppleSkin = builtins.fetchurl { url = "https://cdn.modrinth.com/data/EsAfCjCV/versions/b5ZiCjAr/appleskin-fabric-mc1.21-3.0.6.jar"; sha256 = "18papz1qpjvg9fd91bf5iqvxg1fbdn9fc21a744cf6sb570ww6cs"; };
-    _Collective = builtins.fetchurl { url = "https://cdn.modrinth.com/data/e0M1UDsY/versions/nwmUrrgY/collective-1.21.1-7.87.jar"; sha256 = ""; };
+    _Collective = builtins.fetchurl { url = "https://cdn.modrinth.com/data/e0M1UDsY/versions/nwmUrrgY/collective-1.21.1-7.87.jar"; sha256 = "07cm0vjpa4fyijw31ygs78bidgkzbzc5j2rwkrw5k0w8pw989dzc"; };
     DeathBackup = builtins.fetchurl { url = "https://cdn.modrinth.com/data/Ot5JFxuv/versions/Zq5GkqAd/deathbackup-1.21.1-3.4.jar"; sha256 = "0ca6f5fxh9g5gbpd0iybzbgx5ihjc0cizhs05mqfxzbmzqxz6cbh"; };
     ItemRenameOLD = builtins.fetchurl { url = "https://cdn.modrinth.com/data/i5cjb3PQ/versions/qJaQ6MwF/itemrename-1.0.3%2B1.21.jar"; sha256 = "0si51h2h7spmnzi0x18f188qvhx73c6kbzr2iclrmyflpx0kqf85"; name="itemrename-1.0.32B1.21.jar";};
     Lithium = builtins.fetchurl { url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/frXUdgvL/lithium-fabric-0.14.2-snapshot%2Bmc1.21.1-build.90.jar"; sha256 = "115a6dham2bnwwqsljp68za3vwb9r9zql7dbdk5d0xdgsq6vzb2f"; name = "lithium-fabric-0.14.2-snapshot2Bmc1.21.1-build.90.jar"; };
     SimpleVoiceChat = builtins.fetchurl { url = "https://cdn.modrinth.com/data/9eGKb6K1/versions/Fku4RjPN/voicechat-fabric-1.21.1-2.5.26.jar"; sha256 = "0zzwdsx5763aazm3j0qzaz4m58cxcinw3kzmhwjr62r17kcg82vl"; };
-    Spark = builtins.fetchurl { url = "https://cdn.modrinth.com/data/l6YH9Als/versions/cALUj9l1/spark-1.10.109-fabric.jar"; sha256 = ""; };
-    Xaero'sMiniMap = builtins.fetchurl { url = "https://cdn.modrinth.com/data/1bokaNcj/versions/C6gFqr7V/Xaeros_Minimap_24.6.1_Fabric_1.21.jar"; sha256 = ""; };
-    Xaero'sWorldMap = builtins.fetchurl { url = "https://cdn.modrinth.com/data/NcUtCpym/versions/373K4YJh/XaerosWorldMap_1.39.0_Fabric_1.21.jar"; sha256 = ""; };
+    Spark = builtins.fetchurl { url = "https://cdn.modrinth.com/data/l6YH9Als/versions/cALUj9l1/spark-1.10.109-fabric.jar"; sha256 = "0ivhfnkparn91lca3vdy9jx3ai6m8yp1qyjbwwr1rarxqld15z7s"; };
+    Xaero'sMiniMap = builtins.fetchurl { url = "https://cdn.modrinth.com/data/1bokaNcj/versions/C6gFqr7V/Xaeros_Minimap_24.6.1_Fabric_1.21.jar"; sha256 = "1v1zz1kr943xpj1swwj5i4xq9s0vfzglf1cx21y1pl7yb8ix0xmj"; };
+    Xaero'sWorldMap = builtins.fetchurl { url = "https://cdn.modrinth.com/data/NcUtCpym/versions/373K4YJh/XaerosWorldMap_1.39.0_Fabric_1.21.jar"; sha256 = "1xilzwgdv9f39756hlfz4c15s98n7hnnlhm58nwgq7g82czymvf1"; };
 }
