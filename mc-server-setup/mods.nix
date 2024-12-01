f0de117fd583f12cc9b7f6f1e98531bbe5f1f02e
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2024-12-01 10:04:54 +0000

    itemrename filename

--- mc-server-setup/mods.nix
+++ mc-server-setup/mods.nix
@@ -3,7 +3,7 @@
     AppleSkin = builtins.fetchurl { url = "https://cdn.modrinth.com/data/EsAfCjCV/versions/b5ZiCjAr/appleskin-fabric-mc1.21-3.0.6.jar"; sha256 = "18papz1qpjvg9fd91bf5iqvxg1fbdn9fc21a744cf6sb570ww6cs"; };
     _Collective = builtins.fetchurl { url = "https://cdn.modrinth.com/data/e0M1UDsY/versions/nwmUrrgY/collective-1.21.1-7.87.jar"; sha256 = ""; };
     DeathBackup = builtins.fetchurl { url = "https://cdn.modrinth.com/data/Ot5JFxuv/versions/Zq5GkqAd/deathbackup-1.21.1-3.4.jar"; sha256 = "0ca6f5fxh9g5gbpd0iybzbgx5ihjc0cizhs05mqfxzbmzqxz6cbh"; };
-    ItemRenameOLD = builtins.fetchurl { url = "https://cdn.modrinth.com/data/i5cjb3PQ/versions/qJaQ6MwF/itemrename-1.0.3%2B1.21.jar"; sha256 = ""; };
+    ItemRenameOLD = builtins.fetchurl { url = "https://cdn.modrinth.com/data/i5cjb3PQ/versions/qJaQ6MwF/itemrename-1.0.3%2B1.21.jar"; sha256 = ""; name="itemrename-1.0.32B1.21.jar";};
     Lithium = builtins.fetchurl { url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/frXUdgvL/lithium-fabric-0.14.2-snapshot%2Bmc1.21.1-build.90.jar"; sha256 = ""; name = "ithium-fabric-0.14.2-snapshot2Bmc1.21.1-build.90.jar"; };
     Spark = builtins.fetchurl { url = "https://cdn.modrinth.com/data/l6YH9Als/versions/cALUj9l1/spark-1.10.109-fabric.jar"; sha256 = ""; };
     SimpleVoiceChat = builtins.fetchurl { url = "https://cdn.modrinth.com/data/9eGKb6K1/versions/Fku4RjPN/voicechat-fabric-1.21.1-2.5.26.jar"; sha256 = ""; };
